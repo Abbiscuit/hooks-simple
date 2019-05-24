3b579a5c68727f8ebe5be72d1d80eee284ef41bb
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-05-24 10:41:59 +0900

    Add simple class-based component first

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    resources: ""
+  };
+  render() {
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.setState({ resources: "posts" })}>
+            Posts
+          </button>
+          <button onClick={() => this.setState({ resources: "todos" })}>
+            Todos
+          </button>
+        </div>
+        {this.state.resources}
+      </div>
+    );
+  }
+}
+
+export default App;
