0459f8fba3fb7a4e7596ef27d900c00a58f3a98a
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-05-24 13:09:12 +0900

    Render list with hooks

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,29 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends React.Component {
-  state = {
-    resources: []
-  };
-  async componentDidMount() {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
-    );
-    this.setState({ resources: response.data });
-  }
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
+  useEffect(() => {
+    (async resource => {
       const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+        `https://jsonplaceholder.typicode.com/${resource}`
       );
-      this.setState({ resources: response.data });
-    }
-  }
 
-  render() {
-    return <div>{this.state.resources.length}</div>;
-  }
-}
+      setResources(response.data);
+    })(resource);
+  }, [resource]);
+
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default ResourceList;
